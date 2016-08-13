34617258a51b7bb0837fbe5d8db8831edc3b4f1e
ohyusheng <humbleoh@gmail.com>
2016-08-14 01:16:22 +0800

    handle watchdog timer under running mode and power saving mode

--- embedded/watchdog_timer_running_plus_powersaving.ino
+++ embedded/watchdog_timer_running_plus_powersaving.ino
@@ -4,11 +4,55 @@
 #include <avr/power.h>
 #include <avr/interrupt.h>
 
-#define PINLED 13
+const int pin_led = 13;
+int v = 'R';
+
+boolean is_standby = true;
+
+ISR(WDT_vect) {
+  Serial.print('I');
+  //wdt_reset();
+  //是否要reboot, 关键决策在这里...
+
+  if (is_standby) {
+    wdt_reset();
+  } else {
+    // eg. must be rebooted
+    // configure
+    MCUSR = 0;                          // reset flags
+       
+                                        // Put timer in reset-only mode:
+    WDTCSR |= 0b00011000;               // Enter config mode.
+    WDTCSR =  0b00001000 | 0b000000;    // clr WDIE (interrupt enable...7th from left)
+                                        // set WDE (reset enable...4th from left), and set delay interval
+                                        // reset system in 16 ms...
+                                        // unless wdt_disable() in loop() is reached first                             
+    // reboot
+    while(1);
+  }
+}
+
+void configure_wdt() {
+  // disable interrupt
+  cli();
+
+  // reset status register flags
+  MCUSR = 0;
+  
+  // enter into configure mode
+  WDTCSR |= 0b00011000;
+
+  // enable interrupt, reset disabled, set timeout to 8 seconds
+  WDTCSR = 0b1000000 | 0b00100001;
+
+  // re-enable interrupts
+  sei();
+}
 
 void setup() {
   // put your setup code here, to run once:
   wdt_disable();
+  
   Serial.begin(9600);
   Serial.print('S');
   wdt_enable(WDTO_4S);
@@ -16,7 +60,30 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.read();
-  Serial.print('r');
-  delay(1000);
+  if (Serial.available()) {
+    int t = Serial.read();
+    if (t == 'I') {
+      wdt_disable();
+      configure_wdt();
+      v = 'I';
+    } else if (t == 'R') {
+      wdt_disable();
+      wdt_enable(WDTO_4S);
+      v = 'R';
+    }
+  }
+
+  if (v == 'I') {
+    Serial.print('i');
+    delay(1000);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_mode(); // how to wake up??
+                  //  only when interrrupt event happens, the execution will leave from here
+    sleep_disable();
+    power_all_enable();
+  } else if (v == 'R') {
+    Serial.print('r');
+    wdt_reset();
+    delay(1000);
+  }
 }
