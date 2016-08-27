1563f989e42d72b07aba866d15a71aec52a21e25
ohyusheng <humbleoh@gmail.com>
2016-08-27 22:21:58 +0800

    first time commit file pwm_measurement.ino

---
+++ embedded/pwm_measurement.ino
@@ -0,0 +1,39 @@
+volatile int r1 = 0;
+volatile int f1 = 0;
+volatile int pulseWidth = 0;
+volatile int period = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(9, OUTPUT);
+  analogWrite(9, 254);
+
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), test, RISING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void test() {
+  attachInterrupt(digitalPinToInterrupt(2), test2, FALLING);
+  int r2 = micros();
+  if (r1 > 0) {
+    period = r2 - r1;
+    pulseWidth = f1 - r1;
+    unsigned int dutyCycle = (int32_t) pulseWidth * 255 / period;
+    Serial.println(dutyCycle);
+    r1 = r2;
+  } else {
+    r1 = r2;
+  }
+}
+
+void test2() {
+  attachInterrupt(digitalPinToInterrupt(2), test, RISING);
+  f1 = micros();
+}
+
